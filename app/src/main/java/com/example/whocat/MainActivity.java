322da38a4b928d577773dc1d476860530b16666e
danie <danie@DESKTOP-M2H76EP>
2019-04-07 17:58:07 +0200

    Open random cat gif webpage

--- app/src/main/java/com/example/whocat/MainActivity.java
+++ app/src/main/java/com/example/whocat/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.whocat;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.content.Context;
 import android.os.Bundle;
@@ -89,6 +90,21 @@ public class MainActivity extends AppCompatActivity
 //        for (String catBreed : catBreeds) {
 //            mCatBreedsTextView.append(catBreed + "\n\n\n");
 
+    /**
+     * This method is called when the Open Website button is clicked. It will open the website
+     * specified by the URL represented by the variable urlAsString using implicit Intents.
+     *
+     * @param v Button that was clicked.
+     */
+    public void onClickOpenWebpageButton(View v){
+        // COMPLETED (5) Create a String that contains a URL ( make sure it starts with http:// or https:// )
+        String urlAsString = "https://randomcatgifs.com/";
+
+        // COMPLETED (6) Replace the Toast with a call to openWebPage, passing in the URL String from the previous step
+        openWebPage(urlAsString);
+
+    }
+
         private void makeSearchQuery() {
             String catQuery = mSearchBoxEditText.getText().toString();
             URL catSearchUrl = NetworkUtils.buildUrl(catQuery);
@@ -123,8 +139,10 @@ public class MainActivity extends AppCompatActivity
         // First, hide the currently visible data
         mSearchResultsTextView.setVisibility(View.INVISIBLE);
         // Then, show the error
-        mErrorMessageDisplay.setVisibility(View.VISIBLE);
+
+
     }
+
 
 
     // Create a class called QueryTask that extends AsyncTask<URL, Void, String>
@@ -227,7 +245,7 @@ public class MainActivity extends AppCompatActivity
 //        mToast = Toast.makeText(this, toastMessage, Toast.LENGTH_LONG);
 //
 //        mToast.show();
-        // COMPLETED (1) Retrieve the text from the EditText and store it in a variable
+        // Retrieve the text from the EditText and store it in a variable
         /* We'll first get the text entered by the user in the EditText */
         String textEntered = mSearchResultsTextView.getText().toString();
 
@@ -261,6 +279,39 @@ public class MainActivity extends AppCompatActivity
          */
         startActivity(startChildActivityIntent);
 
+    }
+
+    // COMPLETED (1) Create a method called openWebPage that accepts a String as a parameter
+    /**
+     * This method fires off an implicit Intent to open a webpage.
+     *
+     * @param url Url of webpage to open. Should start with http:// or https:// as that is the
+     *            scheme of the URI expected with this Intent according to the Common Intents page
+     */
+    private void openWebPage(String url) {
+        // COMPLETED (2) Use Uri.parse to parse the String into a Uri
+        /*
+         * We wanted to demonstrate the Uri.parse method because its usage occurs frequently. You
+         * could have just as easily passed in a Uri as the parameter of this method.
+         */
+        Uri webpage = Uri.parse(url);
+
+        // COMPLETED (3) Create an Intent with Intent.ACTION_VIEW and the webpage Uri as parameters
+        /*
+         * Here, we create the Intent with the action of ACTION_VIEW. This action allows the user
+         * to view particular content. In this case, our webpage URL.
+         */
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+        // COMPLETED (4) Verify that this Intent can be launched and then call startActivity
+        /*
+         * This is a check we perform with every implicit Intent that we launch. In some cases,
+         * the device where this code is running might not have an Activity to perform the action
+         * with the data we've specified. Without this check, in those cases your app would crash.
+         */
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
 }
